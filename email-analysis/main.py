11fe8adb3336efd938f12e2ddbcade3e20a0445c
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2013-03-17 01:04:06 +0200

    added the main program, which now only creates a cache out of emails

---
+++ email-analysis/main.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+import os
+import email
+import pickle
+
+def get_msgs(maildir, cachefile):
+    """
+    TODO: Write me
+    """
+    mails = []
+    maildir = os.path.join(os.path.abspath(os.path.expanduser(maildir)), 'cur')
+    cachefile = os.path.abspath(os.path.expanduser(cachefile))
+
+    print(maildir, cachefile)
+
+    try:
+        with open(cachefile, 'rb') as ch:
+            print('from cache')
+            return pickle.load(ch)
+
+    except IOError:
+        for f in os.listdir(maildir):
+            with open(os.path.join(maildir, f), 'rb') as fp:
+                mails.append(email.message_from_binary_file(fp))
+
+        with open(cachefile, 'wb') as ch:
+            print('to cache')
+            pickle.dump(mails, ch)
+
+        return mails
+
+
+if __name__ == '__main__':
+    # TODO: config file that contains the maildir path and the cache path
+    msgs = []
+    path = os.path.expanduser('~/Maildir/yap/phpro-book')
+    msgs = get_msgs(path, 'emails.cache')
+
+    print(len(msgs))
