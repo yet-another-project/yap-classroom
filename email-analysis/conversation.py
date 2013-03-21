2a63e11fa2f45ff633145f8606d0fc60fbac2edf
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2013-03-21 23:14:13 +0200

    Conversation now holds a refs list and doesn't do an insertion sort anymore

--- email-analysis/conversation.py
+++ email-analysis/conversation.py
@@ -1,25 +1,12 @@
-import bisect
-import email
-
-
-def comp(a, b):
-    t1 = email.utils.parsedate_to_datetime(a['Date']).timestamp()
-    t2 = email.utils.parsedate_to_datetime(b['Date']).timestamp()
-
-    return t1 < t2
-
-
 class Conversation(object):
-    def __init__(self, message=None, key=comp):
+    def __init__(self, message=None):
         """Create a conversation by optionally adding message to it
 
         The key parameter should be a callback similar to comp(a, b), ie. it
         must be usabe as the __lt__ method of email.message.Message
         """
         self.msgs = []
-        self.in_reply_tos = []
-        self.messages_id = []
-        self.key = key
+        self.refs = []
 
         if message:
             self.add_msg(message)
@@ -32,15 +19,15 @@ class Conversation(object):
         of the existing messages is a reply to it, also the message is a part
         of the conversation if it references any existing message
         """
-        if message['In-Reply-To'] in self.messages_id or \
-            message['Message-Id'] in self.in_reply_tos:
+        if message['In-Reply-To'] in self.refs or \
+            message['Message-Id'] in self.refs:
             return True
 
         if 'References' not in message.keys():
             return False
 
         for ref in message['References'].split():
-            if ref in self.messages_id:
+            if ref in self.refs:
                 return True
 
         return False
@@ -48,11 +35,12 @@ class Conversation(object):
     def add_msg(self, message):
         # using list extend because RFC 822, 4021 and 2822  specify that the
         # In-Reply-To header may contain multiple addresses
-        if 'In-Reply-To' in message.keys(): #TODO: debug here
-            self.in_reply_tos.append(message['In-Reply-To'])
+        if 'In-Reply-To' in message.keys():
+            self.refs.extend(message['In-Reply-To'].split())
 
-        self.messages_id.append(message['Message-Id'])
+        if 'References' in message.keys():
+            self.refs.extend(message['References'].split())
 
-        # allow bisect.insort() to compare messages
-        email.message.Message.__lt__ = self.key
-        bisect.insort(self.msgs, message)
+        self.refs.append(message['Message-Id'])
+
+        self.msgs.append(message)
